6f0d9ca5afca0f204efb10f6700cdc4220c91778
Gary Ditsch <garyditsch@gmail.com>
2016-10-10 11:08:44 -0400

    adding filter by complete status

--- app.js
+++ app.js
@@ -5,13 +5,14 @@
 angular.module('app', [])
     .controller('TodoController', TodoController)
 
+        
 function TodoController() {
     const ctrl = this;
         
-
     ctrl.taskList = [];
     ctrl.showingForm = true;
     ctrl.showingSaveNewTaskButton = true;
+    ctrl.completeFilterStatus = false;
    
     // Define input values, and reset function.
     ctrl.taskModel = {
@@ -35,12 +36,14 @@ function TodoController() {
     }
 
     // This function allows user to complete task
-    ctrl.completeTask = function(index) {
+    ctrl.completeTask = function(task) {
+        var index = ctrl.taskList.indexOf(task)
         ctrl.taskList[index].completed = true;
     };
 
     // this function allow user to uncomplete task
-    ctrl.uncompleteTask = function(index) {
+    ctrl.uncompleteTask = function(task) {
+        var index = ctrl.taskList.indexOf(task)
         ctrl.taskList[index].completed = false;
     };
 
@@ -49,6 +52,38 @@ function TodoController() {
         ctrl.taskList.splice(index, 1);     
     }
 
+
+    // this function populate form with task data so user can edit
+    ctrl.editTask = function(index) {
+        ctrl.taskModel = {
+            task: ctrl.taskList[index].task,
+        }
+    };
+
+    // this function will return the array of all or filtered by complete status
+    ctrl.getFilteredByComplete = function() {
+        if (ctrl.completeFilterStatus === false) {
+            return ctrl.taskList;
+        } else {
+            ctrl.taskList2 = [];
+            for (var i=0; i < ctrl.taskList.length; i++) {
+                if (ctrl.taskList[i].completed === false) {
+                    ctrl.taskList2.push(ctrl.taskList[i]);
+                }
+            }
+
+            return ctrl.taskList2;
+        } 
+    }
+
+    // toggle for turning completeFiterStatus to true
+    ctrl.completeFilterStatusButton = function() {
+        ctrl.completeFilterStatus = true;
+    }
+
+    ctrl.allFilterStatusButton = function() {
+        ctrl.completeFilterStatus = false;
+    }
 
     // Add a new task to taskList on new form input.
     ctrl.addNewTask = function() {
@@ -62,5 +97,6 @@ function TodoController() {
         ctrl.taskList.push(task);
         ctrl.clearForm()
     };
+
 
 }; // end TodoController
